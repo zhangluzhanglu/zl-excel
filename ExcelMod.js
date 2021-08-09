b42e3b014a86e984a120d5f3059ac9af839b5046
zhangluzhanglu <1758658975@qq.com>
2021-08-10 00:39:47 +0800

    [feat]优化代码,添加可以直接导出到服务器本地磁盘的逻辑

--- ExcelMod.js
+++ ExcelMod.js
@@ -3,47 +3,68 @@
 const Excel = require('exceljs');
 class ExcelMod {
     /**
-          * @function 生成表格对象的功能函数
-          * @description 接收一个数组或对象，生成含有一个或者多个sheet的Excel表格
-          * @param frameInfo {Object} 表示此表格将在何种框架中使用。如：{ name:"egg.js",filename:"测试表格",other:ctx}
-          * @param frameInfo.name {Object} 表示此模块在哪个框架信息中使用
-          * @param frameInfo.name {String} 导出的表格文件名
-          * @param frameInfo.other {Object} 此框架需要的其他信息对象集合
-          * @param params {Object|Array} 对象或者对象数组，传入对象表示只创建一个sheet,对象数组表示创建多个sheet
-          * @return workbook表格对象
-          * @author 张路 2021/08/09
-          * @example
-          * 参数一：框架信息【可选】
-          * { name:"egg.js",filename:"测试表格",other:ctx}
-          *
-          * 参数二：表格数据信息【必填】
-          * [
-          *    {
-          *       sheetName: '用户表', //第一个sheet的名字
-          *       columns: [
-          *                 { header: '姓名', key: 'name', width: 15 },
-          *                 { header: '性别', key: 'sex', width: 10 },
-          *                 { header: '年龄', key: 'age', width: 20, default: 0 },
-          *                 { header: '爱好', key: 'hobby', width: 15, default: 0 },
-          *            ],
-          *        rows: [
-          *                { name: "张三", sex: "男", age: 18, hobby: "小说、音乐"},
-          *                { name: "李四", sex: "女", age: 19, hobby: "小说、音乐、学习"}
-          *             ]
-          *     }
-          *  ]
-        */
-    static async getWorkbook(frameInfo, params) {
-        const workbook = new Excel.Workbook();
+      * @function 生成表格对象的功能函数
+      * @description 接收表格相关信息，生成含有一个或者多个sheet的Excel表格
+      *
+      * @param paramsObj {Object} 完整的表格参数信息
+      * @param paramsObj.excelData {Object|Array} 生成的excel表格数据，为对象或者对象数组，传入对象表示只创建一个sheet,对象数组表示创建多个sheet
+      * @param paramsObj.excelPath {String} 生成的excel表格路径
+      * @param paramsObj.frameInfo {Object} 表示此表格将在何种框架中使用。如：{ name:"egg.js",filename:"测试表格",other:ctx}
+      * @param paramsObj.frameInfo.filename {String} 表示生成的ecel名字
+      * @param paramsObj.frameInfo.name {String} 表示此模块在哪个框架信息中使用
+      * @param paramsObj.frameInfo.other {Object} 此框架需要的其他信息对象集合
+      * @param paramsObj.returnCall {Boolean} 将表格对象返回到调用处,默认为false即不返回
+      * @return workbook表格对象
+      * @author 张路 2021/08/09
+      * @example
+      * {
+      *    // 表格数据信息【必填】
+      *    excelData:[
+      *     {
+      *       sheetName: '用户表', //第一个sheet的名字
+      *       columns: [
+      *        { header: '姓名', key: 'name', width: 15 },
+      *        { header: '性别', key: 'sex', width: 10 },
+      *        { header: '年龄', key: 'age', width: 20, default: 0 },
+      *        { header: '爱好', key: 'hobby', width: 15, default: 0 },
+      *       ],
+      *       rows: [
+      *        { name: "张三", sex: "男", age: 18, hobby: "小说、音乐"},
+      *        { name: "李四", sex: "女", age: 19, hobby: "小说、音乐、学习"}
+      *       ]
+      *     }
+      *   ],
+      *   //框架信息【可选】
+      *   frameInfo:{ name:"egg.js",filename:"测试表格",other:ctx},
+      * }
+    */
+    static async getWorkbook(paramsObj) {
+        const { excelPath, excelData, frameInfo, returnCall = false } = paramsObj;
+        let workbook;
+        // 传了表格路径，但是没有传入框架信息，则表示 表格将生成在本地硬盘
+        if (excelPath && !frameInfo) {
+            workbook = new Excel.stream.xlsx.WorkbookWriter({
+                filename: excelPath,
+            });
+        }
+        // 每传表格路径，但是传入了框架信息，则表示 表格将通过路由返回到前端页面
+        if (!excelPath && frameInfo) {
+            workbook = new Excel.Workbook();
+        }
+        // 其他情况默认构建new Excel.Workbook();，如只传了表格数据的情况
+        else {
+            workbook = new Excel.Workbook();
+        }
+        // 具体的表格信息
         workbook.creator = 'zl-table';
         workbook.created = new Date(Date.now());
         workbook.modified = new Date(Date.now());
         // 传入多个sheet对象构成的数组
         let options;
-        if (Object.prototype.toString.call(params) == '[object Object]') {
-            options = [params]; // 表示传入的对象
-        } else if (Array.isArray(params)) {
-            options = params; // 表示传入的数组
+        if (Object.prototype.toString.call(excelData) == '[object Object]') {
+            options = [excelData]; // 表示传入的对象
+        } else if (Array.isArray(excelData)) {
+            options = excelData; // 表示传入的数组
         } else {
             throw new Error('excel配置参数格式错误');
         }
@@ -89,27 +110,42 @@ class ExcelMod {
                 sheet.addRow(rowData);
             });
         }
-        // console.log('==========frameInfo', frameInfo);
-        // 如果没有传入任何的框架信息，那么就直接直接返回Excel对象
-        if (!frameInfo) {
+        // ==================表格生成完毕，开始处理==================
+        // 没有传入框架信息，但是传了表格路径，则表示 表格将生成在本地硬盘
+        if (excelPath && !frameInfo) {
+            // 直接生成表格，不返回到调用处
+            if (!returnCall) {
+                workbook.commit();
+                return;
+            }
+            // 将表格信息返回到调用处，供二次加工处理，然后手动调用commit();
+            if (returnCall) {
+                return workbook;
+            }
+        }
+        // 如果路径和框架信息都没传，只传入了表格数据，那么就必然是直接返回Excel对象到调用处
+        if (!excelPath && !frameInfo) {
             return workbook;
         }
-        // egg.js框架
-        else if (frameInfo.name == 'egg.js') {
-            const { other, filename } = frameInfo;
-            if (!other || !filename) {
-                throw frameInfo.name + '必须传入ctx和filename';
+        // 如果只传了数据和框架信息，那么必然是直接将表格通过路由响应到前端
+        else if (!excelPath && frameInfo) {
+            // egg.js框架
+            if (frameInfo.name == 'egg.js') {
+                const { other, filename } = frameInfo;
+                if (!other || !filename) {
+                    throw frameInfo.name + '必须传入ctx和filename';
+                }
+                const ctx = other;
+                // 表格信息请求头
+                ctx.set('content-disposition', `attachment; filename* = UTF-8''${encodeURIComponent(filename)}`);
+                ctx.status = 200;
+                await workbook.xlsx.write(ctx.res);
+                ctx.res.end();
             }
-            const ctx = other;
-            // 表格信息请求头
-            ctx.set('content-disposition', `attachment; filename* = UTF-8''${encodeURIComponent(filename)}`);
-            ctx.status = 200;
-            await workbook.xlsx.write(ctx.res);
-            ctx.res.end();
-        }
-        // 其他
-        else {
-            console.log('暂不支持出egg.js之外的其他框架自动构建导出');
+            // 其他
+            else {
+                console.log('暂不支持出egg.js之外的其他框架自动构建导出');
+            }
         }
     }
 }
